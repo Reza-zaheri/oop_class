6644202c4673de4988086cf270007a3cf11ee685
Reza Zaheri <90191604+Reza-zaheri@users.noreply.github.com>
2024-05-07 22:57:08 +0330

    tamrin 01

---
+++ tamrin 01/extention_method/extention_method/ex_num.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace extention_method
+{
+    public static class ex_num
+    {
+        public static string GetSum(this number n)
+        {
+            int sum = n.num1 + n.num2;
+            return sum.ToString();
+        }
+    }
+}
---
+++ tamrin 01/extention_method/extention_method/number.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace extention_method
+{
+    public class number
+    {
+        private int n1;
+        public int num1 { get { return n1; } set { n1 = value; } }
+        private int n2;
+        public int num2 { get { return n2; } set { n2 = value; } }
+    }
+}
