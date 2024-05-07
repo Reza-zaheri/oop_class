6048432ce9050e924fdb3de8cbefc1626f433085
Reza Zaheri <90191604+Reza-zaheri@users.noreply.github.com>
2024-05-07 23:11:31 +0330

    tamrin 01

---
+++ tamrin 01/extention_method/extention_method/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace extention_method
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                number num = new number();
+                num.num1 = int.Parse(textBox1.Text);
+                num.num2 = int.Parse(textBox2.Text);
+                label4.Text = num.GetSum();
+            }
+            catch
+            {
+                label3.Text = "لطفا عدد وارد کنید";
+                label3.ForeColor = Color.Red;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                number num = new number();
+                num.num1 = int.Parse(textBox1.Text);
+                num.num2 = int.Parse(textBox2.Text);
+                label4.Text = num.minus();
+            }
+            catch
+            {
+                label3.Text = "لطفا عدد وارد کنید";
+                label3.ForeColor = Color.Red;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                number num = new number();
+                num.num1 = int.Parse(textBox1.Text);
+                num.num2 = int.Parse(textBox2.Text);
+                label4.Text = num.multi();
+            }
+            catch
+            {
+                label3.Text = "لطفا عدد وارد کنید";
+                label3.ForeColor = Color.Red;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                number num = new number();
+                num.num1 = int.Parse(textBox1.Text);
+                num.num2 = int.Parse(textBox2.Text);
+                label4.Text = num.divid();
+            }
+            catch
+            {
+                label3.Text = "لطفا عدد وارد کنید";
+                label3.ForeColor = Color.Red;
+            }
+        }
+    }
+}
---
+++ tamrin 01/extention_method/extention_method/ex_num.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace extention_method
+{
+    public static class ex_num
+    {
+        public static string GetSum(this number n)
+        {
+            int sum = n.num1 + n.num2;
+            return sum.ToString();
+        }
+        public static string minus(this number n)
+        {
+            int sum = n.num1 - n.num2;
+            return sum.ToString();
+        }
+        public static string multi(this number n)
+        {
+            int sum = n.num1 * n.num2;
+            return sum.ToString();
+        }
+        public static string divid(this number n)
+        {
+            int sum = n.num1 / n.num2;
+            return sum.ToString();
+        }
+    }
+}
